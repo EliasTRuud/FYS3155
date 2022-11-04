b735dbb3720837d93136f7dedb391fd23e5afbf7
EliasTRuud <elias.tidemandruud@gmail.com>
2022-11-04 14:42:50 +0100

    All files

---
+++ Proj2 Update/neuralnetwork.py
@@ -0,0 +1,8 @@
+from scratch import gradient_decent
+import numpy as np
+
+#Class neuralnetowrk, array of layers. Backprop function. Feed forward function.
+#Class layer, stores weights in matrix. Stores index of layer, activation function.
+
+
+#Backprop, computing the gradients.
