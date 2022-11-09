de0db200739854fe2e05ae06211952985ba0e461
EliasTRuud <elias.tidemandruud@gmail.com>
2022-11-09 12:19:56 +0100

    Attempt failed on backprop

--- Proj2 Update/NN_v2.py
+++ Proj2 Update/NN_v2.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
+np.random.seed(12)
+
 class Layer:
     def __init__(self, prevLayer, n_nodes, sigma, simga_d, bias=0.1):
         self.n_nodes = n_nodes
         self.prevLayer = prevLayer
-        
+
         if isinstance(prevLayer, Layer):
             self.n_weights = prevLayer.n_nodes
         else:
@@ -24,6 +26,9 @@ class Layer:
     def init_bias(self, bias):
         self.bias = np.zeros(self.n_nodes) + bias
 
+    def __str__(self):
+        return f"{self.z.shape}, {self.weights.shape}, {self.bias.shape}"
+
     @property
     def get_bias(self):
         return self.bias
@@ -31,7 +36,7 @@ class Layer:
     @get_bias.setter
     def get_bias(self, bias):
         self.bias = bias
-    
+
     @property
     def get_weights(self):
         return self.weights
@@ -79,37 +84,36 @@ class NeuralNetwork:
         #     self.n_outputs = Y_data.shape[1]
         # else:
         #     self.n_outputs = 1
-        
-
-
 
         #initializing layers
         if isinstance(n_nodes, int):
             self.layers = [Layer(self.n_features, n_nodes, sigma, sigma_d)]
-            for i in range(1, n_layers+1):
+            for i in range(1, n_layers):
                 self.layers.append(Layer(self.layers[i-1], n_nodes, sigma, sigma_d))
             # self.output_layer = Layer(self.layers[i-1], self.n_outputs, sigma, sigma_d)
-            self.layers.append(Layer(self.layers[i-1], self.n_outputs, sigma, sigma_d))
+            self.layers.append(Layer(self.layers[n_layers-1], self.n_outputs, sigma, sigma_d))
         else:
             self.layers = [Layer(self.n_features, n_nodes[0], sigma, sigma_d)]
-            for i in n_nodes[1:]:
-                self.layers.append(Layer(self.layers[i-1], i, sigma, sigma_d))
+            for i,n in enumerate(n_nodes[1:]):
+                self.layers.append(Layer(self.layers[i-1], n, sigma, sigma_d))
             # self.output_layer = Layer(self.layers[i-1], self.n_outputs, sigma, sigma_d)
             self.layers.append(Layer(self.layers[i-1], self.n_outputs, sigma, sigma_d))
-
 
         self.epochs = epochs
         self.batch_size = batch_size
         self.iterations = self.n_inputs // self.batch_size
+        #self.iterations  = 2000
         self.eta = eta
         self.lmbd = lmbd
-
 
     def feedForward(self):
         layer1 = self.layers[0]
         weights = layer1.get_weights
         bias = layer1.get_bias
 
+        #print(self.X_data.shape)
+        #print(weights.shape)
+        #print(bias.shape)
         #print(np.shape(bias), np.shape(weights), np.shape(self.X_data))
         #z = np.matmul(weights, self.X_data) + bias
         # z = np.matmul(self.X_data, weights) + bias
@@ -133,7 +137,7 @@ class NeuralNetwork:
             layer.get_a = layer.sigma(z)
             a.append(layer.get_a)
 
-        self.output = a[-1]
+        self.output = a[-1] # (batch_size, nr_of_output_nodes=1), for regression
         #self.a = np.array(a)
 
     def feedForwardOut(self, X):
@@ -154,7 +158,7 @@ class NeuralNetwork:
         #     z = al
 
         z = np.matmul(X, weights) + bias
-        print(z.shape, weights.shape, bias.shape)
+        #print(z.shape, weights.shape, bias.shape)
         layer1.get_z = z
         layer1.get_a = layer1.sigma(z)
         a = [layer1.get_a]
@@ -163,35 +167,106 @@ class NeuralNetwork:
             weights = layer.get_weights
             bias = layer.get_bias
             z = np.matmul(a[-1], weights) + bias
-            print(z.shape, weights.shape, bias.shape)
+            #print(z.shape, weights.shape, bias.shape)
             layer.get_z = z
             layer.get_a = layer.sigma(z)
             a.append(layer.get_a)
 
         return z
 
-    def backProp(self):
+    def backProp_(self):
         Y_data = self.Y_data
 
         error_output = self.output - Y_data
+        #print(error_output.shape)
         error = [error_output]
 
+        # gradients for the output layer
+        out_layer = self.layers[-1]
+        out_weights_grad = np.matmul(layer.prevLayer.get_a.T, error_output)
+
+        outlayer.get_weights = out_layer.get_weights - output_weights_gradient
+        out_layer.get_bias = layer.prevLayer.get_bias - self.eta*error_output
+
         #going through backwards
-        for layer in self.layers[:-1:-1]:
-            error.append(np.matmul(error[-1], layer.prevLayer.get_weights.T)*layer.sigma_d(layer.get_z))
+        for layer in reversed(self.layers[1:]):
+            sigma_der = layer.prevLayer.sigma_d(layer.prevLayer.get_z)
+            error.append(np.matmul(error[-1], layer.get_weights.T)*sigma_der)
             #error.append(np.sum(error[-1]*layer.prevLayer.get_weights*layer.sigma_d(layer.get_z))) #, axis=?
             #weights_grad = self.eta*np.matmul(error[-1].T, layer.prevLayer.get_a)
             #or
             weights_grad = self.eta*np.matmul(layer.prevLayer.get_a.T, error[-1])
-            layer.get_weights = layer.get_weights - weights_grad
-            layer.get_bias = layer.get_bias - self.eta*error[-1]
+            layer.prevLayer.get_weights = layer.prevLayer.get_weights - weights_grad
+            layer.prevLayer.get_bias = layer.prevLayer.get_bias - self.eta*error[-1]
 
-        
+
+    def backProp_v2(self):
+        Y_data = self.Y_data
+
+        error_output = self.output - Y_data
+
+        error = [error_output]
+
+        outLayer = self.layers[-1]
+        w_grad_output = np.matmul(outLayer.prevLayer.get_a.T, error_output)
+        w_grad = [w_grad_output]
+        bias_grad_output = np.sum(error_output, axis=0)
+        bias_grad = [bias_grad_output]
+
+        weights_list = [outLayer.get_weights]
+        bias_list = [outLayer.get_bias]
+
+        #going through backwards
+        for layer in reversed(self.layers[1:]):
+            weights = layer.prevLayer.get_weights
+            bias = layer.prevLayer.get_bias
+            sigma_d = layer.prevLayer.sigma_d
+
+            error.append(np.matmul(error[-1], layer.get_weights.T)*sigma_d(layer.prevLayer.get_z))
+
+            ah = layer.prevLayer.get_a
+            if isinstance(layer.prevLayer.prevLayer, int):
+                ah = self.X_data
+            w_grad.append(np.matmul(ah.T, error[-1]))
+
+            bias_grad.append(np.sum(error[-1], axis=0))
+
+            weights_list.append(weights)
+            bias_list.append(bias)
+
+        for i, layer in enumerate(reversed(self.layers)):
+            print(weights_list[i].shape)
+            #print(f"b4 Layer wei shape {i}: {layer.get_weights.shape}")
+            layer.get_weights = weights_list[i] - self.eta*w_grad[i]
+            layer.get_bias = bias_list[i] - self.eta*bias_grad[i]
+            #print(f"Layer wei shape {i}: {layer.get_weights.shape}")
+
+    def backProp(self):
+        Y_data = self.Y_data
+        error_output = self.output - Y_data
+
+        outLayer = self.layers[-1]
+
+        error_hidden = np.matmul(error_output, outLayer.get_weights.T) * outLayer.get_a * (1 - outLayer.get_a)
+
+        output_weights_gradient = np.matmul(outLayer.prevLayer.get_a.T, error_output)
+        output_bias_gradient = np.sum(error_output, axis=0)
+
+        hidden_weights_gradient = np.matmul(self.X_data.T, error_hidden)
+        hidden_bias_gradient = np.sum(error_hidden, axis=0)
+
+        outLayer.get_weights -= self.eta * output_weights_gradient
+        outLayer.get_bias -= self.eta * output_bias_gradient
+        self.layers[-2].get_weights -= self.eta * hidden_weights_gradient
+        self.layers[-2].get_bias -= self.eta * hidden_bias_gradient
+
+
     def train(self):
         data_indices = np.arange(self.n_inputs)
-        
+
         for i in range(self.epochs):
             for j in range(self.iterations):
+                print(f"j: {j}")
                 #(750,) 100
                 #print(data_indices.shape, self.batch_size)
                 chosen_data_points = np.random.choice(data_indices, size=self.batch_size, replace=False)
@@ -210,10 +285,10 @@ class NeuralNetwork:
 
 
 n = 1000
-x = np.linspace(0, 10, n)
+x = np.linspace(0, 1, n)
 
 def f(x):
-    return 1 + 5*x + 3*x**2 
+    return 1 + 5*x + 3*x**2
 
 y = f(x)
 
@@ -224,16 +299,28 @@ def sigmoid_deriv(x):
     sig_x  = sigmoid(x)
     return sig_x*(1 - sig_x)
 
+def linear(x):
+    return x
+
+def linear_deriv(x):
+    return 1
+
 X_train, X_test, Y_train, Y_test = train_test_split(x, y,test_size=1/4)
 
-dnn = NeuralNetwork(X_train, Y_train, 2, 40, sigmoid, sigmoid_deriv)
+dnn = NeuralNetwork(X_train, Y_train, 2, 32, sigmoid, sigmoid_deriv, epochs = 10, eta = 0.001)
+dnn.layers[-1].sigma = linear
+dnn.layers[-1].sigma_d = linear_deriv
 
+test_predict_untrained = dnn.predict(X_test)
 dnn.train()
 
 test_predict = dnn.predict(X_test)
+
+
 print(test_predict.shape)
 print(Y_test.shape)
 plt.scatter(X_test, Y_test, label="Actual", c="r")
-plt.scatter(X_test, test_predict, label="Model")
+plt.scatter(X_test, test_predict, label="Model", alpha = 0.5)
+plt.scatter(X_test, test_predict_untrained, label="Model_none")
 plt.legend()
 plt.show()
