69d3f56580492434fa657246c17f5e0e92cfb5b0
EliasTRuud <elias.tidemandruud@gmail.com>
2022-12-23 05:49:26 +0100

    new methods

--- Project3/main.py
+++ Project3/main.py
@@ -1,12 +1,19 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import warnings
+warnings.filterwarnings("ignore", message=r"Passing", category=FutureWarning)
 from genResults import get_df, balance_df
 from functions import R2, MSE
-from tensorflow_addons.metrics import RSquare
+#from tensorflow_addons.metrics import RSquare
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
 from functions import mcc
-# from tfa.metrics import MatthewsCorrelationCoefficient 
+# from tfa.metrics import MatthewsCorrelationCoefficient
+from sklearn.metrics import matthews_corrcoef, accuracy_score
+from sklearn.linear_model import LogisticRegression
 import tensorflow as tf
 import seaborn as sns
 import pandas as pd
@@ -52,9 +59,9 @@ def keras_NN(X_train, X_test, y_train, y_test, metrics=["accuracy"], epochs = 10
     it does not work with more than two. The feature takes in four parameters, which are assumed to
     be eta, n_neurons, n_layers and lamda, other variables are not supported.
     """
-    if not(isinstance(metrics, list)): 
+    if not(isinstance(metrics, list)):
         raise TypeError
-    
+
     #define tunable parameters
     if not(isinstance(grid_search, list)):
         parameter1 = np.logspace(-3, -1, 3) #eta
@@ -78,7 +85,7 @@ def keras_NN(X_train, X_test, y_train, y_test, metrics=["accuracy"], epochs = 10
                 parameters_names[j] = parameter_name
                 j += 1
         parameter1, parameter2, parameter3, parameter4 = parameters
-    
+
 
     train_accuracy = np.zeros((len(parameter2), len(parameter1)))
     test_accuracy = np.zeros((len(parameter2), len(parameter1)))
@@ -87,7 +94,7 @@ def keras_NN(X_train, X_test, y_train, y_test, metrics=["accuracy"], epochs = 10
         for j in range(len(parameter1)):
             print("combination: ", j+i*len(parameter1)+1, "out of ", len(parameter1)*len(parameter2),
                   f"\n{parameters_names[0]}={parameter1[j]}, {parameters_names[1]}={parameter2[i]}")
-            DNN_model = NN_model(X_train.shape[1], metrics=metrics, 
+            DNN_model = NN_model(X_train.shape[1], metrics=metrics,
                                 grid_search=[[parameter1[j], parameters_names[0]], [parameter2[i], parameters_names[1]],
                                              [parameter3, parameters_names[2]], [parameter4, parameters_names[3]]]
                                 )
@@ -131,10 +138,96 @@ def single_keras_NN(X_train, X_test, y_train, y_test, eta, lamda, n_neurons, n_l
     fig.subplots_adjust(wspace=0.001)
     plt.show()
 
-if __name__ == "__main__":
+
+def logisticReg(showValue=True):
+    """
+    Using sklearn logistic regression model. Default paramters, which uses L2-penalty equal
+    """
     df = get_df("covid_data.csv", n=10000, balance=True)
     # df = df.sample(n=10000, random_state=seed)
-    print(df["HIGH_RISK"].value_counts()[1], df["HIGH_RISK"].value_counts()[0])
+    low = df["HIGH_RISK"].value_counts()[0]
+    high = df["HIGH_RISK"].value_counts()[1]
+    print(f"Ratio of high vs low risk: high/low = {(high/low):.2f} (its balanced if close to 1)")
+    print(df["HIGH_RISK"].value_counts()[1], df["HIGH_RISK"].value_counts()[0]) #prints HIGH_RISK vs Low risk patients. E.g 4600 6300
+    target = df["HIGH_RISK"]
+    inputs = df.loc[:, df.columns != "HIGH_RISK"]
+
+    X = inputs
+    y = target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+    #accScore = []
+    #mathCoef = []
+    #for i in range():
+
+    clf = LogisticRegression(random_state=11).fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    mcc = matthews_corrcoef(y_test, y_pred)
+
+    confM = confusion_matrix(y_test, y_pred)
+    tn, fp, fn, tp = confM.ravel()
+
+    if showValue:
+        print("Results for logistic regression sklearn.")
+        print(f"Accuracy: {100*acc:.2f}%.  MatthewsCorrelationCoefficient: {mcc:.2f}\n")
+    return acc, mcc, confM
+
+def simpleDecisionTree(showSearch=False, showValue=True):
+    """
+    Gridsearch using decision tree to find best paramaters using accuracy score.
+    Adjusts criterion, max_depth, min_samples_split and min_samples_leaf according to dict.
+    Uses best paramters found to find acc and mcc.
+    """
+    df = get_df("covid_data.csv", n=10000, balance=True)
+
+    target = df["HIGH_RISK"]
+    inputs = df.loc[:, df.columns != "HIGH_RISK"]
+
+    X = inputs
+    y = target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+    if showSearch:
+        param_dict = {"criterion" : ["gini", "entropy"], #default = gini
+                    "max_depth" : [None, 2, 3, 5, 7, 9, 12, 20], #default = None
+                    "min_samples_split" : [2, 4, 6, 8, 10],
+                    "min_samples_leaf" : [1, 2, 4, 6, 8]} #deafult = 2
+        tree = DecisionTreeClassifier(random_state=11)
+
+        grid = GridSearchCV(tree, param_grid = param_dict, n_jobs=-1, verbose=1) #default 5-fold cross validation
+        #n_jobs : Number of jobs to run in parallel. None means 1. -1 means using all processors.
+        grid.fit(X_train, y_train)
+
+        #print(grid.cv_results_)
+        print(grid.best_params_)
+        print(grid.best_estimator_)
+        print(grid.best_score_)
+
+    #Uses results for best params here then calculate acc and mcc again
+    treeBest = DecisionTreeClassifier(criterion = "entropy", max_depth=7, min_samples_split=2, random_state=11, min_samples_leaf=1)
+    treeBest.fit(X_train, y_train)
+
+    y_pred = treeBest.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    mcc = matthews_corrcoef(y_test, y_pred)
+    confM = confusion_matrix(y_test, y_pred)
+    tn, fp, fn, tp = confM.ravel()
+    if showValue:
+        print(f"Result for best decision tree w/max_depth: 7")
+        print(f"Accuracy: {100*acc:.2f}%.  MatthewsCorrelationCoefficient: {mcc:.2f}\n")
+    return acc, mcc, confM
+
+
+if __name__ == "__main__":
+
+    logisticReg()
+    #simpleDecisionTree()
+    """
+    df = get_df("covid_data.csv", n=10000, balance=True)
+    # df = df.sample(n=10000, random_state=seed)
+    print(df["HIGH_RISK"].value_counts()[1], df["HIGH_RISK"].value_counts()[0]) #prints HIGH_RISK vs Low risk patients. E.g 4600 6300
     target = df["HIGH_RISK"]
     inputs = df.loc[:, df.columns != "HIGH_RISK"]
 
@@ -162,9 +255,9 @@ if __name__ == "__main__":
     lamda = np.logspace(-3, 2, 6)
     lamda = 0.01 #lambda
 
-    grid_search = [[eta, "eta"], [n_neurons, "n_neurons"], [n_layers, "n_layers"], [lamda, "lamda"]]    
+    grid_search = [[eta, "eta"], [n_neurons, "n_neurons"], [n_layers, "n_layers"], [lamda, "lamda"]]
 
-    # train_accuracy, test_accuracy = keras_NN(X_train, X_test, y_train, y_test, metrics=metrics, epochs=epochs, 
+    # train_accuracy, test_accuracy = keras_NN(X_train, X_test, y_train, y_test, metrics=metrics, epochs=epochs,
     #                                          batch_size=batch_size, grid_search=grid_search)
 
     # plot_data(train_accuracy, [feature_name for feature, feature_name in grid_search if isinstance(feature, np.ndarray)], "Training")
@@ -174,7 +267,7 @@ if __name__ == "__main__":
     lamda = 0.01
 
     # df_train = balance_df(df[:len(df)//4*3], "HIGH_RISK")
-    
+
     # target = df_train["HIGH_RISK"]
     # inputs = df_train.loc[:, df_train.columns != "HIGH_RISK"]
 
@@ -193,3 +286,4 @@ if __name__ == "__main__":
     # y_test = to_categorical(y_test)
 
     single_keras_NN(X_train, X_test, y_train, y_test, eta, lamda, n_neurons, n_layers, metrics=metrics, epochs=epochs, batch_size=batch_size)
+    """
